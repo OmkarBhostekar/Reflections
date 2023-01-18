58e2a35e3a7dd3dba2bcb24bc9633fb2dfec06de
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-01-19 03:43:24 +0530

    user profile page tage1

---
+++ components/Cards/Cards.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import River from "../../assets/river.jpg"
+type Props = {}
+
+const Cards = (props: Props) => {
+  return (
+    <div>
+        <div className="card-zoom w-full lg:max-w-full lg:flex ">
+          <div
+            className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+            style={{backgroundImage: `url(${River.src})` }}
+            title="River"
+          ></div>
+          <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div className="mb-8">
+              <div className="text-gray-900 font-bold text-xl mb-2">
+                15 Rivers In Norway
+              </div>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="flex items-center">
+              <img
+                className="w-10 h-10 rounded-full mr-4"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+                alt="Avatar of Writer"
+              />
+              <div className="text-sm">
+                <p className="text-gray-900 leading-none">Karen Johnson</p>
+                <p className="text-gray-600">Aug 10</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Cards
--- src/app/user/[uid]/page.tsx
+++ src/app/user/[uid]/page.tsx
@@ -1,9 +1,56 @@
+"use client";
+import Cards from "components/Cards/Cards";
+import Latest from "components/Latest";
+import Recommended from "components/Recommended";
+import UserCard from "components/UserCard";
 import React from "react";
-
 type Props = {};
-
+const user = window.location.pathname.split("/")[2].toString();
+const size = 1;
+const userId = "omkar";
 const Profile = (props: Props) => {
-  return <div>Profile</div>;
+  return (
+      <div className=" flex     ">
+        
+        <div className="space-y-2 pl-10">
+          <UserCard />
+          <h1 className="text-4xl font-bold">Recent Blogs,</h1>
+          <Cards />
+          <Cards />
+          <Cards />
+
+        </div>
+        
+        <div className="w-full space-y-3 pl-10 pr-10 pt-10">
+          <h1 className="text-4xl font-bold">Popular Blogs,</h1>
+          
+          {size === 0 ? (
+              userId === user ? (
+                <>
+                <p className="text-2xl">Your Blogs,</p>
+                <p className="text-2xl">You have not created any blogs yet</p>
+                </>
+              ) : (
+                <>
+                <p className="text-2xl">{user} Blogs,</p>
+                <p className="text-2xl">This user has not created any blogs yet</p>
+                </>
+              ) 
+              
+          ) : (
+            <>
+              {" "}
+              <Cards />
+              <Cards />
+              <Cards />
+              <Cards />
+              <Cards />
+            </>
+          )}
+        </div>
+
+      </div>
+  );
 };
 
 export default Profile;
