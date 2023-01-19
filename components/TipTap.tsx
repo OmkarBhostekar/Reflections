1361c148be1a315e847c0f4b864ac822bb1b5552
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-01-19 13:25:14 +0530

    vaibhav

---
+++ components/TipTap.tsx
@@ -0,0 +1,130 @@
+'use strict'
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
+import { Color } from '@tiptap/extension-color'
+import TextStyle from '@tiptap/extension-text-style'
+
+import './styles.css'
+import {
+  FaBold,
+  FaHeading,
+  FaItalic,
+  FaListOl,
+  FaListUl,
+  FaQuoteLeft,
+  FaRedo,
+  FaStrikethrough,
+  FaUnderline,
+  FaUndo,
+} from "react-icons/fa";
+
+const MenuBar = ({ editor }) => {
+    
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <div className="menuBar">
+      <div>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive("bold") ? "is_active" : ""}
+        >
+          <FaBold />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive("italic") ? "is_active" : ""}
+        >
+          <FaItalic />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+          className={editor.isActive("underline") ? "is_active" : ""}
+        >
+          <FaUnderline />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={editor.isActive("strike") ? "is_active" : ""}
+        >
+          <FaStrikethrough />
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 2 }) ? "is_active" : ""
+          }
+        >
+          <FaHeading />
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 3 }) ? "is_active" : ""
+          }
+        >
+          <FaHeading className="heading3" />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive("bulletList") ? "is_active" : ""}
+        >
+          <FaListUl />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive("orderedList") ? "is_active" : ""}
+        >
+          <FaListOl />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive("blockquote") ? "is_active" : ""}
+        >
+          <FaQuoteLeft />
+        </button>
+      </div>
+      <div>
+        <button onClick={() => editor.chain().focus().undo().run()}>
+          <FaUndo />
+        </button>
+        <button onClick={() => editor.chain().focus().redo().run()}>
+          <FaRedo />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const Tiptap = ({ }) => {
+
+
+  const editor = useEditor({
+    extensions: [StarterKit, Underline,TextStyle, Color],
+    content: ``,
+    
+
+    onUpdate: ({ editor }) => {
+      const html = editor.getHTML();
+      
+      console.log(html);
+    },
+  });
+  return (
+    <div className="textEditor">
+      <MenuBar editor={editor} />
+      <EditorContent className='dark:bg-black dark:text-black' editor={editor} />
+    </div>
+  );
+};
+
+function useState<T>(arg0: string): [any, any] {
+    throw new Error("Function not implemented.");
+}
