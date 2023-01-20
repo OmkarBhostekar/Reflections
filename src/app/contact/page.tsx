d22eddb9528647b97cda8c1bf283f61a7b96d700
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-01-20 05:52:34 +0530

    contact us submit form working successfully

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,4 @@
-'use client'
 import React from "react";
-import { useForm, SubmitHandler, LiteralUnion } from "react-hook-form";
-import {useState} from "react";
-
 interface Data{
   email: string;
   subject: string;
@@ -10,30 +6,7 @@ interface Data{
 }
 type Props = {};
 
-
 const page = (props: Props) => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Data>();
-
-  const onSubmit: SubmitHandler<Data> = (data) => console.log(data);
-  const registerOptions = {
-    name: { required: "Name is required" },
-    email: { required: "Email is required" },
-    password: {
-      required: "Password is required",
-      minLength: {
-        value: 8,
-        message: "Password must have at least 8 characters",
-      },
-    },
-  };
-
-
-
   return (
     <div className="max-w-[1420px] mx-auto flex flex-col justify-center items-center p-4">
       <section className="bg-white dark:bg-gray-900">
@@ -45,7 +18,8 @@ const page = (props: Props) => {
             Got a technical issue? Want to send feedback about a beta feature?
             Need details about our Business plan? Let us know.
           </p>
-          <form action="#" className="space-y-8">
+          <form action="https://formspree.io/f/xeqwopjd"
+  method="POST" className="space-y-8">
             <div>
               <label
                 htmlFor="email"
@@ -56,15 +30,11 @@ const page = (props: Props) => {
               <input
                 type="email"
                 id="email"
+                name="email"
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="name@flowbite.com"
                 required
-                {...register("email", registerOptions.email)}
               />
-              <small className="text-danger">
-                {errors?.email && errors.email.message}
-              </small>
-              
             </div>
             <div>
               <label
@@ -76,10 +46,10 @@ const page = (props: Props) => {
               <input
                 type="text"
                 id="subject"
+                name="subject"
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Let us know how we can help you"
                 required
-                {...register("subject", registerOptions.name)}
               />
             </div>
             <div className="sm:col-span-2">
@@ -91,15 +61,15 @@ const page = (props: Props) => {
               </label>
               <textarea
                 id="message"
+                name="message"
+               
                 rows={6}
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Leave a comment..."
-                {...register("message", registerOptions.name)}
-
               ></textarea>
             </div>
             <button
-              onClick={handleSubmit(onSubmit)}
+              type="submit"
               className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
               Send message
